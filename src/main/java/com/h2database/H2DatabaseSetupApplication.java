df29007c77924cbc3519d26cfaa093fe6c5c8acc
rushikesh1410 <rushikesh.shinde@cognizant.com>
2023-05-18 13:42:08 +0530

    first commit h2 database setup project

---
+++ src/main/java/com/h2database/H2DatabaseSetupApplication.java
@@ -0,0 +1,13 @@
+package com.h2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DatabaseSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DatabaseSetupApplication.class, args);
+	}
+
+}
